6fccaf9ab69d55aceee30f8f3a3438de405f828b
Ragner <djragner@gmail.com>
2019-03-29 20:59:43 -0300

    Sincronizado

--- app_agenda/models.py
+++ app_agenda/models.py
@@ -1,16 +1,28 @@
 from django.db import models
 
 class AgendaPrivada(models.Model):
-    Nome = models.CharField(max_length=64)
-    Compromisso = models.TextField()
+    class Meta:
+        verbose_name = 'Agenda'
+        verbose_name_plural = 'Agenda'
+
+    Nome = models.CharField('Nome do médico', max_length=64)
+    Compromisso = models.TextField('Tipo de compromisso', max_length=128)
     Data = models.DateTimeField()
 
 class AgendaPublica(models.Model):
-    Nome = models.CharField(max_length=64)
-    Compromisso = models.TextField()
+    class Meta:
+        verbose_name = 'Agenda Pública'
+        verbose_name_plural = 'Agenda Pública'
+
+    Nome = models.CharField('Nome do Médico', max_length=64)
+    Compromisso = models.TextField('Tipo de compromisso', max_length=128)
     Data = models.DateTimeField()
 
 class AgendaInstitucional(models.Model):
-    Nome = models.CharField(max_length=64)
-    Feriado = models.CharField(max_length=64)
+    class Meta:
+        verbose_name = 'Agenda Institucional'
+        verbose_name_plural = 'Agenda Institucional'
+
+    Nome = models.CharField('Nome Médico', max_length=64)
+    Feriado = models.CharField('Nome do feriado', max_length=64)
     Data = models.DateField()
