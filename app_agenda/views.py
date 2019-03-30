e6d457b5d2399981999c0828a1d26477870003a6
Ragner <djragner@gmail.com>
2019-03-29 21:23:28 -0300

    Sincronizado

--- app_agenda/views.py
+++ app_agenda/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+
+from .models import AgendaPublica
+
+class HomePageView(ListView):
+    model = AgendaPublica
+    template_name = 'app_agenda/home.html'
+
